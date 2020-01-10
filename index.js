fda3289fb8b2e041494e8e569ea56ff509c82dc1
Valentin <primarh111@mail.ru>
2020-01-10 12:12:15 +0300

    реализован перезапуск, динамические цвета элементов

--- index.js
+++ index.js
@@ -1,22 +1,80 @@
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
+var $time = document.querySelector('#time')
+var $result = document.querySelector('#result')
+var $timeHeader = document.querySelector('#time-header')
+var $resultHeader = document.querySelector('#result-header')
+var $gameTime = document.querySelector('#game-time')
+
 var score = 0
+var isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGameTime)
+
+function show ($el){
+    $el.classList.remove('hide')
+}
+
+function hide ($el){
+    $el.classList.add('hide')
+}
 
 function startGame () {
-    console.log('шалом, епта');
-    $start.classList.add('hide')
+    score = 0
+    setGameTime()
+    $gameTime.setAttribute('disabled', 'true')
+    isGameStarted = true
+    hide($start)
     $game.style.backgroundColor = '#fff'
+
+    var interval = setInterval(function(){
+        var time = parseFloat($time.textContent)
+
+        if(time <= 0){
+            clearInterval(interval)
+            endGame()
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1) //пока вермя не кончилось отнимаем по 100 мили секунд, фиксед обрезает отображение на первой цифре после запятой
+        }
+    }, 100)
 
     renderBox()
 }
 
+function endGame(){
+    isGameStarted = false
+    gameResult()
+    show($start)
+    $game.style.backgroundColor = '#ccc'
+    $game.innerHTML = ''
+    hide($timeHeader)
+    show($resultHeader)
+    $gameTime.removeAttribute('disabled')
+    // $resultHeader.innerHTML = 'Ваш результат:' + score
+}
+
+function setGameTime(){
+    var gameDuration = +$gameTime.value //+ впереди приводит значение к числу, либо метод parseInt
+    $time.textContent = gameDuration.toFixed(1)
+    show($timeHeader)
+    hide($resultHeader)
+}
+
+
+function gameResult() {
+    $result.textContent = score.toString()
+}
+
+
 function handleBoxClick(event) {
+    if(!isGameStarted){
+        return
+    }
     if (event.target.dataset.catch) {
-        renderBox ()
         score++
+        renderBox ()
     }
                 
 }
@@ -28,11 +86,10 @@ function renderBox () {
     var gameSize = $game.getBoundingClientRect()
     var maxTop = gameSize.height - boxSize
     var maxLeft = gameSize.width - boxSize
-    console.log(gameSize.height);
     
     box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
-    box.style.backgroundColor = '#000'
+    box.style.backgroundColor = generateColor()
     box.style.top = getRandom(0, maxTop)  + 'px'
     box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
@@ -43,5 +100,8 @@ function renderBox () {
 
 function getRandom (min, max) {
     return Math.floor(Math.random() * (max - min) + min) 
-
 }
+
+function generateColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16)
+  }
