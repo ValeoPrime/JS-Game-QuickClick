6dbbcef87a8a0f4ccebf9f644f366a2a1d3c3be5
Valentin <primarh111@mail.ru>
2020-01-10 10:01:03 +0300

    реализовано рандомное перемещение квадрата цели по игровому полю

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var score = 0
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
 
-
 function startGame () {
     console.log('шалом, епта');
     $start.classList.add('hide')
@@ -22,20 +21,27 @@ function handleBoxClick(event) {
                 
 }
 
-
 function renderBox () {
     $game.innerHTML = ''
     var box = document.createElement('div')
-
-    box.style.height = box.style.width = '50px'
+    var boxSize = getRandom (20, 80)
+    var gameSize = $game.getBoundingClientRect()
+    var maxTop = gameSize.height - boxSize
+    var maxLeft = gameSize.width - boxSize
+    console.log(gameSize.height);
+    
+    box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
     box.style.backgroundColor = '#000'
-    box.style.top = '20%'
-    box.style.left = '45%'
+    box.style.top = getRandom(0, maxTop)  + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
     box.setAttribute('data-catch', 'true') // добавили дата атрибут для отслеживания кликов
     
-
-
     $game.insertAdjacentElement('afterbegin', box)  //вставляем box  внутрь $game
+}
+
+function getRandom (min, max) {
+    return Math.floor(Math.random() * (max - min) + min) 
+
 }
